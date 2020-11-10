39cff551aa4072433309bb1a4d4dbd7b874b2891
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-10 21:39:39 +0000

    Changed pseudocode

--- mod_1.py
+++ mod_1.py
@@ -12,10 +12,9 @@ if __name__ == "__main__": # it checks whether the code is run from current file
 # Soooo for those wondering "why?" well if you wanna debug a file, lets say
 # our main.py is the place where we do all the calls for python snake we made.
 # We can make another __name__ == "__main__": in another file and in that
-# if statement we can create funky shit we need debugging that
+# if statement we can create funky code we need debugging that
 # will not be ran if we import but it will run when we run that specific file
 # the file you run py file_name.py is always __main__
 # file you import
 # lets say from python import Python
 # its __name__ == python
-# Testing
